9475407736d9621854f771b57eb5e17661d934b0
yodahyun <133837667+yodahyun@users.noreply.github.com>
2023-05-19 15:59:09 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,4 +1,5 @@
-import math
+#import math
+
 def solution(price):
     if price >= 500000:
         price = price*(80/100)
@@ -6,4 +7,6 @@ def solution(price):
         price = price*(90/100)
     elif price >= 100000:
         price = price*(95/100)
-    return math.trunc(price)
+        
+    #return math.trunc(price) 소수점 버리기
+    return int(price)
